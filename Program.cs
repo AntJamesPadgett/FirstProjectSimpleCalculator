e02aea4c4eef260a71d12e7ae27ec175dd3f1b34
antjamespadgett <padgettanthonyj@gmail.com>
2021-11-02 21:21:47 -0500

    added another num

--- Program.cs
+++ Program.cs
@@ -10,10 +10,12 @@ namespace SimpleCalculator
             double num1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter another number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num3 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(num1/num2);
-
+            Console.WriteLine(num1+num2*num3);
             Console.ReadLine();
+
         }
     }
 }
